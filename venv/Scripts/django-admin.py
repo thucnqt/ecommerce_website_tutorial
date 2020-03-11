1ce6b3fff9a9f66ad7fb5bc6c75b81072122c72e
Knowledge <thucnqt@gmail.com>
2020-03-12 03:48:38 +0700

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Knowledge\PycharmProjects\ecommerce_website_tutorial\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
